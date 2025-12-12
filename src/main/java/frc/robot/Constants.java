3866f6df21e93da7e802fddfa47078f4de502efe
Kaan <kaannkahramannn@gmail.com>
2025-12-13 01:02:17 +0300

    update

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,38 +17,37 @@ public final class Constants {
         public static final double ANGLE_GEAR_RATIO = 150.0 / 7.0; 
         public static final double WHEEL_DIAMETER = Units.inchesToMeters(4.0);
         
-        // Dönüşüm Faktörleri (Encoder'dan metreye veya radyana)
         public static final double DRIVE_POS_FACTOR = (WHEEL_DIAMETER * Math.PI) / DRIVE_GEAR_RATIO;
-        public static final double DRIVE_VEL_FACTOR = DRIVE_POS_FACTOR / 60.0; // RPM -> m/s
-        public static final double ANGLE_POS_FACTOR = (2 * Math.PI) / ANGLE_GEAR_RATIO; // Rotasyon -> Radyan
+        public static final double DRIVE_VEL_FACTOR = DRIVE_POS_FACTOR / 60.0; // rpm -> m/s
+        public static final double ANGLE_POS_FACTOR = (2 * Math.PI) / ANGLE_GEAR_RATIO; // rotasyon -> radyan
 
-        // --- CAN ID'leri ---
-        // Pigeon 2.0
+        // --- CAN ID ---
+        // Pigeon
         public static final int PIGEON_ID = 20;
 
         // Front Left
         public static final int FL_DRIVE_ID = 2;
         public static final int FL_ANGLE_ID = 3;
         public static final int FL_CANCODER_ID = 21;
-        public static final double FL_OFFSET = -Math.toRadians(0.0); // Cancoder Offseti (Dereceyi radyana çevir)
+        public static final double FL_OFFSET = 0.0; // cancoder Offset derece -> radyan
 
         // Front Right
         public static final int FR_DRIVE_ID = 4;
         public static final int FR_ANGLE_ID = 5;
         public static final int FR_CANCODER_ID = 22;
-        public static final double FR_OFFSET = -Math.toRadians(0.0);
+        public static final double FR_OFFSET = 0.0;
 
         // Back Left
         public static final int BL_DRIVE_ID = 6;
         public static final int BL_ANGLE_ID = 7;
         public static final int BL_CANCODER_ID = 23;
-        public static final double BL_OFFSET = -Math.toRadians(0.0);
+        public static final double BL_OFFSET = 0.0;
 
         // Back Right
         public static final int BR_DRIVE_ID = 8;
         public static final int BR_ANGLE_ID = 9;
         public static final int BR_CANCODER_ID = 24;
-        public static final double BR_OFFSET = -Math.toRadians(0.0);
+        public static final double BR_OFFSET = 0.0;
 
 
         public static final double ANGLE_P = 0.01; 
